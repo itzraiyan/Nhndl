8bbb10643356062c1596701bc702b2cf85a6db90
Zilhazz Arefin <mdzilhazzarefin@gmail.com>
2025-07-11 12:06:48 +0600

    Update naming.py

--- core/naming.py
+++ core/naming.py
@@ -6,9 +6,3 @@ def safe_filename(name, max_len=150):
     if len(name) > max_len:
         name = name[:max_len].rstrip()
     return name
-
-def make_gallery_filename(meta, config):
-    tpl = config.get("filename_template", "{id} - {title}")
-    title = meta.get("title", meta.get("id", "unknown"))
-    title = safe_filename(title, config.get("max_filename_len", 150))
-    return tpl.format(id=meta.get("id"), title=title)
