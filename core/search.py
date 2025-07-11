0842cb60d6f497926092e5572dafe5415310b37c
Zilhazz Arefin <mdzilhazzarefin@gmail.com>
2025-07-11 11:43:15 +0600

    Create search.py

---
+++ core/search.py
@@ -0,0 +1,8 @@
+# Stub for search logic and tag filtering
+def search_nhentai(keyword, page=1, tags=None, exclude_tags=None):
+    # TODO: Implement real search logic using nhentai API, add tag filter/exclude
+    # Return fake result for now
+    return [
+        {"id": "123456", "title": "Test Gallery 1"},
+        {"id": "654321", "title": "Test Gallery 2"}
+    ]
